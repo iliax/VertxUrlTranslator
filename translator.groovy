60ab889426a99702ee05ddeeae6efc0af9864ee7
BigBlackBug <bigblackbugg@gmail.com>
2013-01-08 22:34:00 +0400

    refactored

--- translator.groovy
+++ translator.groovy
@@ -37,19 +37,14 @@ def sendData(def req, def url){
 	URLConnection conn = new URL(url).openConnection()
 	conn.setConnectTimeout(10000)
 	conn.setReadTimeout(20000)
-	
-	BufferedInputStream inn = new BufferedInputStream(conn.getInputStream())
-	def data = new byte[1024]
-	int count
-	def buff = new org.vertx.groovy.core.buffer.Buffer()
-	while ((count = inn.read(data, 0, 1024)) != -1) {
-		def bytes = new byte[count]
-		System.arraycopy(data, 0, bytes, 0, count)	//refactor it then
-		buff.appendBytes((byte[]) bytes)
-	}
+
+	def bytes = new ByteArrayOutputStream()
+	bytes << new URL(url).openStream()
+	bytes.close()
 	
 	req.response.putHeader("Content-Type", conn.getContentType())
-	req.response.putHeader("Content-Length", buff.getLength())
+	req.response.putHeader("Content-Length", bytes.size())
+	def buff = new org.vertx.groovy.core.buffer.Buffer(bytes.toByteArray())
 	req.response.end(buff)
 }
 
